bd501aac6bd88c01585a2718b0d797ddbebb2bfc
Ghast1PT <rafaelcardoso2003@gmail.com>
2023-07-13 20:32:31 +0100

    show book

--- resources/js/components/pages/Book.jsx
+++ resources/js/components/pages/Book.jsx
@@ -11,7 +11,7 @@ function Book() {
             try {
                 const response = await axios.get(`/livro/${id}`);
                 setBook(response.data);
-    
+
             } catch (error) {
                 console.error(error);
             }
@@ -19,11 +19,24 @@ function Book() {
         fetchData();
     }, [id]);
 
-    
 
     return (
-        <div>{book.title}</div>
+        <div className='mt-40'>
+            <div className="grid-cols-2 grid-flow-row grid gap-x-96 mb-24">
+                <div className="">
+                    <h1 className='text-3xl font-bold'>{book.title}</h1>
+                    <h2 className='mt-4 text-lg text-gray-800'>{book.author}</h2>
+                </div>
+                <div className='text-right'>
+                    <a className='bg-third p-4 rounded-lg text-white'>Requisitar</a>
+                </div>
+            </div>
+            <div>
+                <p className='text-slate-600'>{book.sinopse}</p>
+            </div>
+        </div>
     )
+
 }
 
 export default Book
